e4e4b84447c3fcd9e3a7144d87c507d55cda975b
Jasper Lambrects <lambrechts.jasper@gmail.com>
2019-07-08 16:58:19 +0200

    Start of the project.

--- src/Controller/Parser.java
+++ src/Controller/Parser.java
@@ -4,15 +4,22 @@ import Model.*;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Parser {
 
     private Scanner scan;
 
-    private String ADline;
-    private String[] AMLines;
-    private String blockCommand = "%";
+    private String line;
+
+    private List<String> ADcommand = new ArrayList<>();
+    private List<String> AMCommand = new ArrayList<>();
+    private List<List> commandList = new ArrayList<>();
+
+    private final static String blockCommand = "%";
+    private StringBuilder templine;
 
     private Settings settings = new Settings();
     private File file;
@@ -21,38 +28,43 @@ public class Parser {
     public Parser(File file) {
         this.file = file;
         this.filelocation = file.getParent();
-
     }
 
     public void parse() {
         try {
             scan = new Scanner(file);
-            scan.delimiter("%");
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+
         System.out.println("Loaded the following file " + file + " and start parsing ");
+
         while (scan.hasNext()) {
-            String line = scan.next();
+            line = scan.next();
+            System.out.println(line);
 
-            if (!line.substring(0,1).equals(blockCommand)) {
-
+            if (line.substring(0, 1).equals(blockCommand) && line.substring(line.length() - 1).equals(blockCommand)) {
+                parseAdCommand();
+            } else if (line.substring(0, 1).equals(blockCommand) && !line.substring(line.length() - 1).equals(blockCommand)) {
+                parseAmCommand();
             }
-            //Check for AD command
-
-            //check for AM command
-
-            //check for Settings command
-
-            //check for polygons
-
-
-
-            //getSettings(line);
-            //parsePads(line);
-            //parseMacro(line);
-            //parsePolygons(line);
         }
+    }
+
+    private void parseAmCommand() {
+        while (!line.substring(line.length() - 1).equals(blockCommand)) {
+            templine.append(line);
+            line = scan.next();
+        }
+        AMCommand.add(line);
+        System.out.println("AM COMMAND EXECUTED" + AMCommand);
+
+
+    }
+
+    private void parseAdCommand() {
+        ADcommand.add(line);
+        System.out.println("ADD COMMAND EXECUTED" + ADcommand);
     }
 }
